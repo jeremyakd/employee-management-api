4aec7a3d0333fc60732a99baa430a66dcb3452eb
jeremyakd <jeremiasc@exo.com.ar>
2021-11-30 08:58:30 -0300

    Se agrega documentacion con Swagger.

--- app.py
+++ app.py
@@ -20,6 +20,21 @@ mongo = PyMongo(app)
 
 db = mongo.db
 
+
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Register employees"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
+
+
 # ROUTES 
 
 ######################################################## ALTA ########################################################
